326ede53ae2a89e9add184a1b9507d49379e2eb6
javawithlwl <javawithlwl@gmail.com>
2024-03-06 06:28:42 +0530

    Added sum natural numbers

---
+++ src/main/java/com/careerit/jfs/sample/SumOfNaturalNumbers.java
@@ -0,0 +1,12 @@
+package com.careerit.jfs.sample;
+
+public class SumOfNaturalNumbers {
+
+    public static void main(String[] args) {
+
+            int n = 10;
+            int sum = 0;
+            sum = n * (n + 1) / 2;
+            System.out.println("The sum of first " + n + " natural numbers is " + sum);
+    }
+}
